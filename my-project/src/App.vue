33c6cba4da608390214cbf3987eeffc0eea02421
陈辉 <ch41618@ly.com>
2017-10-31 15:36:58 +0800

    pc Signed-off-by: 陈辉 <ch41618@ly.com>

---
+++ my-project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+  	<img src="../static/shouye/shouye.png"/>
+  	<div class="people">
+  			<silder></silder>
+  	</div>
+    <!--<router-view/>-->
+  </div>
+</template>
+
+<script>
+import silder from '@/components/silder'	
+export default {
+  name: 'app',
+  components: {
+			silder
+		},
+}
+</script>
+
+<style>
+	#app{
+		background: #f0e5d1;
+	}
+	.people{
+		margin-top: 20px;
+		width: 90%;
+		margin: 0 auto;
+	}
+</style>
